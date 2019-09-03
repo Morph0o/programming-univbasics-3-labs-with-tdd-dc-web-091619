dfd670bdee248ea3c8ae5014e3379d955766b883
collaborative-bug-2010 <detrodoniv@yahoo.com>
2019-09-03 16:17:51 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,8 @@
 def calculator
   first_number = 5
-  second_number=9.0/2
+  second_number= 9.0
   sum = first_number + second_number
   difference = first_number - second_number
-  quotient = first_number/second_number
+  quotient = first_number / second_number
 end
   
