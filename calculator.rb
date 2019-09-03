8a4fdfbfdb2e1811908d05ffffd8ab6531bcfee8
collaborative-bug-2010 <detrodoniv@yahoo.com>
2019-09-03 16:27:52 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,4 +2,5 @@
   second_number= 9.0
   sum = first_number + second_number
   difference = first_number - second_number
+  product = first_number * second_number
   quotient = first_number / second_number
